8c001eb1e1803e04dd6f53ec42ff9f605b2b58bc
corestate55 <manabu.hagiwara@okinawaopenlabs.org>
2022-09-28 17:34:51 +0900

    Add debug option

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 # NOTICE: export PYTHONPATH="./src"
-import sys
-
 from src.crpd_route_table import CrpdRouteTable
 from src.crpd_ospfneigh_table import CrpdOspfNeighborTable
 from src.batfish_route_table import BatfishRouteTable
@@ -10,23 +8,24 @@ import src.utility as util
 import argparse
 import json
 import os
+import sys
 from typing import Dict
 
 
 def cross_check(crpd_table: StateTable, bf_table: StateTable) -> Dict:
     result = {"both": [], "only_crpd": [], "only_bf": []}
-    for bf_rt_entry in bf_table.entries:
-        crpd_rt_entry = crpd_table.find_entry_equiv(bf_rt_entry)
-        if crpd_rt_entry:
-            result["both"].append({"crpd_entry": crpd_rt_entry.to_dict(), "bf_entry": bf_rt_entry.to_dict()})
+    for bf_table_entry in bf_table.entries:
+        crpd_table_entry = crpd_table.find_entry_equiv(bf_table_entry)
+        if crpd_table_entry:
+            result["both"].append({"crpd_entry": crpd_table_entry.to_dict(), "bf_entry": bf_table_entry.to_dict()})
         else:
-            result["only_bf"].append(bf_rt_entry.to_dict())
+            result["only_bf"].append(bf_table_entry.to_dict())
 
-    for crpd_rt_entry in crpd_table.entries:
-        bf_rt_entry = bf_table.find_entry_equiv(crpd_rt_entry)
-        if bf_rt_entry:
+    for crpd_table_entry in crpd_table.entries:
+        bf_table_entry = bf_table.find_entry_equiv(crpd_table_entry)
+        if bf_table_entry:
             continue
-        result["only_crpd"].append(crpd_rt_entry.to_dict())
+        result["only_crpd"].append(crpd_table_entry.to_dict())
 
     return result
 
@@ -35,9 +34,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Cross check routing table")
     parser.add_argument("--config", "-c", type=str, default="config.json", help="config file")
     parser.add_argument("--table", "-t", required=True, choices=["route", "ospfneigh"], help="select target state table")
+    parser.add_argument("--debug", action='store_true', help="raw data to debug")
     args = parser.parse_args()
 
-    if not args.config:
+    if not os.path.exists(os.path.expanduser(args.config)):
         util.error("config file not found")
 
     with open(os.path.expanduser(args.config), "r") as config_file:
@@ -45,23 +45,18 @@ if __name__ == "__main__":
 
         if args.table == "route":
             crpd_rt = CrpdRouteTable(config_data["dev_env"]["ospf_routes_file"])
-            crpd_rt.expand_rt_entry()
-            # print(json.dumps(crpd_rt.to_dict()))
-
             bf_rt = BatfishRouteTable(config_data["sim_env"]["ospf_routes_file"])
-            # print(json.dumps(bf_rt.to_dict()))
-
-            # print(json.dumps([crpd_rt.to_dict(), bf_rt.to_dict()]))
-            print(json.dumps(cross_check(crpd_rt, bf_rt)))
+            if args.debug:
+                print(json.dumps([crpd_rt.to_dict(), bf_rt.to_dict()]))
+            else:
+                crpd_rt.expand_rt_entry()
+                print(json.dumps(cross_check(crpd_rt, bf_rt)))
             sys.exit(0)
 
         if args.table == "ospfneigh":
             crpd_ospfneigh = CrpdOspfNeighborTable(config_data["dev_env"]["ospf_neighbors_file"])
-            # print(json.dumps(crpd_ospfneigh.to_dict()))
-
             bf_ospfneigh = BatfishOspfNeighborTable(config_data["sim_env"]["ospf_neighbors_file"])
-            # print(json.dumps(bf_ospfneigh.to_dict()))
-
-            # print(json.dumps([crpd_ospfneigh.to_dict(), bf_ospfneigh.to_dict()]))
-            print(json.dumps(cross_check(crpd_ospfneigh, bf_ospfneigh)))
-            sys.exit(0)
+            if args.debug:
+                print(json.dumps([crpd_ospfneigh.to_dict(), bf_ospfneigh.to_dict()]))
+            else:
+                print(json.dumps(cross_check(crpd_ospfneigh, bf_ospfneigh)))
